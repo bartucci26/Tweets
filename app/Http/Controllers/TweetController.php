aad55115997ff632fc034f6a2f00d49a13b52b94
Lucas Bartucci <you@example.com>
2021-06-14 20:18:41 +0100

    Inertia installed+TweetController

---
+++ app/Http/Controllers/TweetController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tweet;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class TweetController extends Controller
+{
+    public function index() {
+        $tweets = Tweet::with('user')->get();
+
+        return Inertia::render('Tweets/Index', [
+            'tweets' => $tweets
+        ]);
+    }
+}
